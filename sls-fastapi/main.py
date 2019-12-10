b47b30068c1bfcd23b9b9a73e75c423fe6542725
Jaehyeon Kim <jakim@networkten.com.au>
2019-12-10 06:26:48 +0000

    api up

--- sls-fastapi/main.py
+++ sls-fastapi/main.py
@@ -1,23 +1,11 @@
 import os
-from functools import wraps
-from datetime import datetime
 from fastapi import FastAPI, HTTPException
 
-from src import BasicError, Status, TodoPatch, TodoPost, Item, Items, TodoItems
+from src import BasicError, Status, ChangeItem, NewItem, Item, Items, TodoItems
 
 todos = TodoItems()
 
 app = FastAPI(title="To do list backend")
-
-
-def check_item(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        todo_id = kwargs["todo_id"]
-        print("todo id in decorator - ", todo_id)
-        return f(*args, **kwargs)
-
-    return decorated
 
 
 @app.get("/", response_model=Status, tags=["health"])
@@ -27,28 +15,35 @@ async def health_check():
 
 @app.get("/items", response_model=Items, tags=["todo"])
 async def get_to_do_items():
-    print(todos.items)
     return todos.items
 
 
-@app.post("/item", response_model=Item, tags=["todo"], responses={404: {"model": BasicError}})
-async def create_to_do_item(*, req: TodoPost):
-    return {"id": 1, "todo": "foo bar baz", "username": "John Doe", "created_at": datetime.now()}
-
-
-@app.get("/item/{todo_id}", tags=["todo"])
-# @check_item
+@app.get(
+    "/item/{todo_id}", response_model=Item, tags=["todo"], responses={404: {"model": BasicError}}
+)
 async def get_to_do_item(todo_id: int):
-    print("todo id in get method - ", todo_id)
-    print(todos.item(todo_id))
-    # return todos.item(todo_id)
+    try:
+        return todos.item(todo_id)
+    except StopIteration:
+        raise HTTPException(404, detail="Item not found")
 
 
 @app.delete("/item/{todo_id}", response_model=Status, tags=["todo"])
 async def delete_to_do_item(todo_id: int):
-    return {"status": "ok"}
+    todos.delete_item(todo_id)
+    return {"status": "deleted"}
 
 
-@app.patch("/item/{todo_id}", response_model=Status, tags=["todo"])
-async def patch_to_do_item(todo_id: int, req: TodoPatch):
-    return {"status": "ok"}
+@app.patch(
+    "/item/{todo_id}", response_model=Item, tags=["todo"], responses={404: {"model": BasicError}}
+)
+async def patch_to_do_item(todo_id: int, item: ChangeItem):
+    try:
+        return todos.patch_item(todo_id, item.todo)
+    except StopIteration:
+        raise HTTPException(404, detail="Item not found")
+
+
+@app.post("/item", response_model=Item, tags=["todo"])
+async def create_to_do_item(*, item: NewItem):
+    return todos.create_item(item)
--- sls-fastapi/src.py
+++ sls-fastapi/src.py
@@ -1,6 +1,6 @@
 from typing import List
 from datetime import datetime
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
 
 
 class BasicError(BaseModel):
@@ -11,47 +11,43 @@ class Status(BaseModel):
     status: str
 
 
-class TodoPatch(BaseModel):
+class ChangeItem(BaseModel):
     todo: str
 
 
-class TodoPost(TodoPatch):
-    username: str = Field("John Doe")
+class NewItem(ChangeItem):
+    username: str = "John Doe"
 
 
 class Item(BaseModel):
     id: int
     todo: str
-    username: str
-    created_at: datetime
-    updated_at: datetime = Field(None)
+    username: str = "John Doe"
+    created_at: datetime = datetime.utcnow()
+    updated_at: datetime = None
 
 
 class Items(BaseModel):
-    items: List[Item] = []
+    records: List[Item] = []
 
 
 class TodoItems:
     def __init__(self):
-        self.items = [
-            {
-                "id": 1,
-                "todo": "Watch Parasite",
-                "username": "John Doe",
-                "created_at": datetime.utcnow(),
-            }
-        ]
+        self.items = Items(records=[Item(id=1, todo="Watch Parasite")])
 
-    def item(self, id):
-        return next(i for i in self.items if i["id"] == id)
+    def item(self, todo_id: int):
+        return next(r for r in self.items.records if r.id == todo_id)
 
-    def create_item(self, item: TodoPost):
-        new_item = {
-            "id": len(self.items) + 1,
-            "todo": item.todo,
-            "username": item.username,
-            "created_at": datetime.utcnow(),
-        }
-        self.items.append(new_item)
+    def create_item(self, item: NewItem):
+        new_item = Item(id=len(self.items.records) + 1, todo=item.todo, username=item.username)
+        self.items.records.append(new_item)
         return new_item
 
+    def patch_item(self, todo_id: int, todo: str):
+        item = next(r for r in self.items.records if r.id == todo_id)
+        item.todo = todo
+        item.updated_at = datetime.utcnow()
+        return item
+
+    def delete_item(self, todo_id: int):
+        self.items.records = [r for r in self.items.records if r.id != todo_id]
