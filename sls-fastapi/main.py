3a44b3e272cec8978580cd2898505a205c69f4eb
Jaehyeon Kim <jakim@networkten.com.au>
2019-12-11 21:47:32 +0000

    todoitems updated

--- sls-fastapi/main.py
+++ sls-fastapi/main.py
@@ -1,7 +1,7 @@
 from typing import Union
 from fastapi import FastAPI, HTTPException
 
-from src import BasicError, Status, NewItem, UpdateItem, Item, Items, TodoItems
+from src import BasicError, Status, NewItem, PatchItem, Item, Items, TodoItems, set_dt_field
 
 app = FastAPI(title="To do list backend")
 
@@ -11,7 +11,12 @@ async def health_check():
     return {"status": "ok"}
 
 
-@app.get("/item", response_model=Union[Item, Items], tags=["todo"])
+@app.get(
+    "/items",
+    response_model=Union[Item, Items],
+    tags=["todo"],
+    description="Get item or items\n\taaa",
+)
 async def get_to_do_items(username: str, created_at: str, all_items: bool = False):
     return TodoItems().get_items(username, created_at, all_items)
 
@@ -21,15 +26,16 @@ async def create_to_do_item(item: NewItem):
     return TodoItems().create_item(item)
 
 
-@app.put("/item", response_model=Item, tags=["todo"], responses={404: {"model": BasicError}})
-async def update_to_do_item(item: UpdateItem):
+@app.patch("/item", response_model=Item, tags=["todo"], responses={404: {"model": BasicError}})
+async def update_to_do_item(item: PatchItem):
+    if not TodoItems().is_exist(item.username, item.created_at):
+        raise HTTPException(404, detail="Item not found")
     return TodoItems().update_item(item)
 
 
 @app.delete("/item", response_model=Status, tags=["todo"], responses={404: {"model": BasicError}})
 async def delete_to_do_item(username: str, created_at: str):
-    item = TodoItems().get_items(username, created_at, all_items=False)
-    if len(item["item"]) == 0:
+    if not TodoItems().is_exist(username, created_at):
         raise HTTPException(404, detail="Item not found")
     TodoItems().delete_item(username, created_at)
     return {"status": "deleted"}
--- sls-fastapi/src.py
+++ sls-fastapi/src.py
@@ -1,9 +1,17 @@
 import os
 from typing import List, Union
 from datetime import datetime
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 import boto3
 from boto3.dynamodb.conditions import Key
+
+
+def set_dt_field(is_optional: bool):
+    desc = "Datetime string, format - eg) 2019-01-01T12:00:00Z"
+    if is_optional:
+        return Field(None, description=desc)
+    else:
+        return Field(..., description=desc)
 
 
 class BasicError(BaseModel):
@@ -17,17 +25,20 @@ class Status(BaseModel):
 class NewItem(BaseModel):
     username: str
     todo: str
+    when_at: str = set_dt_field(is_optional=True)
 
 
-class UpdateItem(NewItem):
-    created_at: datetime
+class PatchItem(NewItem):
+    todo: str = None
+    created_at: str = set_dt_field(is_optional=False)
 
 
 class ItemFields(BaseModel):
     todo: str
     username: str
-    created_at: str
-    updated_at: str
+    when_at: str = None
+    created_at: str = set_dt_field(is_optional=False)
+    updated_at: str = set_dt_field(is_optional=False)
 
 
 class Item(BaseModel):
@@ -35,16 +46,41 @@ class Item(BaseModel):
 
 
 class Items(BaseModel):
-    items: List[ItemFields] = []
+    items: Union[List[ItemFields], list]
 
 
 class TodoItems:
     def __init__(self):
-        self.endpoint_url = os.getenv("LOCAL_STACK_ENDPOINT_URL", None)
+        self.endpoint_url = os.getenv("LOCAL_STACK_ENDPOINT_URL")
         self.tablename = os.environ["TABLE_NAME"]
         self.dt_format = "%Y-%m-%dT%H:%M:%SZ"
-        self.hash_key = "username"
-        self.range_key = "created_at"
+
+    def get_items(self, username: str, created_at: str, all_items: bool = False):
+        if all_items:
+            args = {
+                "KeyConditionExpression": Key("username").eq(username)
+                & Key("created_at").gte(created_at),
+                "ScanIndexForward": False,
+            }
+            resp = self._table().query(**args)
+            return {"items": resp.get("Items", [])}
+        else:
+            resp = self._table().get_item(Key={"username": username, "created_at": created_at})
+            return {"item": resp.get("Item", {})}
+
+    def create_item(self, item: NewItem):
+        args = {"todo": item.todo, "username": item.username, "is_create": True}
+        return self._upsert_item(**args)
+
+    def update_item(self, item: PatchItem):
+        args = {"todo": item.todo, "username": item.username, "created_at": item.created_at}
+        return self._upsert_item(**args)
+
+    def delete_item(self, username: str, created_at: str):
+        self._table().delete_item(Key={"username": username, "created_at": created_at})
+
+    def is_exist(self, username: str, created_at: str):
+        return len(TodoItems().get_items(username, created_at, all_items=False)["item"]) > 0
 
     def _table(self):
         if not self.endpoint_url:
@@ -57,35 +93,33 @@ class TodoItems:
                 endpoint_url=self.endpoint_url,
             ).Table(self.tablename)
 
-    def get_items(self, username: str, created_at: str, all_items: bool = False):
-        if all_items:
-            args = {
-                "KeyConditionExpression": Key(self.hash_key).eq(username)
-                & Key(self.range_key).gte(created_at),
-                "ScanIndexForward": False,
-            }
-            resp = self._table().query(**args)
-            return {"items": resp.get("Items", [])}
+    def _upsert_item(self, **kwargs):
+        dt = datetime.utcnow().strftime(self.dt_format)
+        created_at = kwargs["created_at"] if kwargs.get("created_at") else dt
+        if kwargs.get("is_create"):
+            self._table().put_item(
+                Item={
+                    "todo": kwargs["todo"],
+                    "username": kwargs["username"],
+                    "created_at": created_at,
+                    "updated_at": dt,
+                }
+            )
         else:
-            resp = self._table().get_item(Key={"username": username, "created_at": created_at})
-            return {"item": resp.get("Item", {})}
-
-    def create_item(self, item: NewItem):
-        created_at = datetime.utcnow().strftime(self.dt_format)
-        self._table().put_item(
-            Item={
-                "todo": item.todo,
-                "username": item.username,
-                "created_at": created_at,
-                "updated_at": created_at,
+            args = {
+                **{"Key": {"username": kwargs["username"], "created_at": created_at}},
+                **self._set_expr_values({**kwargs, **{"updated_at": dt}}),
             }
-        )
-        return self.get_items(item.username, created_at, all_items=False)
+            self._table().update_item(**args)
+        return self.get_items(kwargs["username"], created_at, all_items=False)
 
-    def update_item(self, item: UpdateItem):
-        return {}
-
-    def delete_item(self, username: str, created_at: str):
-        self._table().delete_item(
-            Key={"username": username, "created_at": created_at,}
+    def _set_expr_values(self, d):
+        keys = ["todo", "when_at", "updated_at"]
+        expr = "set {0}".format(
+            ", ".join(["{0} = :{0}value".format(k) for k, _ in d.items() if k in keys])
         )
+        expr_values = {}
+        for k, _ in d.items():
+            if k in keys:
+                expr_values.update({":{0}value".format(k): d[k]})
+        return {"UpdateExpression": expr, "ExpressionAttributeValues": expr_values}
